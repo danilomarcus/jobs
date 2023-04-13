d4781f1cb0c251ec8dcd9ba20f27e0c1faa197da
Danilo Marcus <danpayne16@msn.com>
2023-04-13 02:05:28 -0300

    nova linha 1 e 2

--- Teste.java
+++ Teste.java
@@ -1,7 +1,8 @@
 class Teste{
  
   public static void main(String args[]){
-    System.out.println("nice build dude 22");
+    System.out.println("nice build dude 1");
+    System.out.println("nice build dude 2");
   }
 }
   
